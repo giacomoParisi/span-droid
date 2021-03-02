c809b41cd7d45e50978a9fdf72fc211f4d1bd806
Giacomo Parisi <giacomoparisi95@gmail.com>
2021-03-02 16:51:23 +0100

    Release 0.2.2

--- buildSrc/src/main/java/AndroidCofig.kt
+++ buildSrc/src/main/java/AndroidCofig.kt
@@ -3,8 +3,8 @@ object AndroidConfig {
     const val min_sdk = 23
     const val target_sdk = 29
     const val compile_sdk = 29
-    const val version_code = 130
-    const val version_name = "0.2.1"
+    const val version_code = 131
+    const val version_name = "0.2.2"
 
     const val test_instrumentation_runner = "androidx.test.runner.AndroidJUnitRunner"
 
